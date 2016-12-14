b2662f5f6a9ddbf65c13d592e58ed6a3a741234d
Matej <vuki146@gmail.com>
2016-12-14 14:52:16 +0100

    Removed force unwrapping and ";" . Added input data validation.

--- Introduction/ViewController.swift
+++ Introduction/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+//    guard https://thatthinginswift.com/guard-statement-swift/
+//    http://ericcerney.com/swift-guard-statement/
 
     @IBOutlet weak var firstNameEtxt: UITextField!
     @IBOutlet weak var lastNameEtxt: UITextField!
@@ -18,22 +21,54 @@ class ViewController: UIViewController {
     @IBAction func clearFields(_ sender: UIButton) {
         
         print("\(sender.currentTitle) button tap!")
-        firstNameEtxt.text="";
-        lastNameEtxt.text="";
-        emailAddressEtxt.text=""
+        firstNameEtxt.text = "";
+        lastNameEtxt.text = "";
+        emailAddressEtxt.text = ""
         
     }
     
     
     @IBAction func submitFields(_ sender: UIButton) {
-        let fName = firstNameEtxt.text;
-        let lName = lastNameEtxt.text;
-        let email = emailAddressEtxt.text
+ 
+        //Bouncer Pattern
+        guard let fName = firstNameEtxt.text else {
+            return
+        }
+        guard let lName = lastNameEtxt.text else{
+            return
+        }
+        guard let email = emailAddressEtxt.text else {
+            return
+        }
+        
+        
+        if isDataOk(fName: fName, lName: lName, email: email){
+            print("First name is: " + fName)
+            print("Last name is: " + lName)
+            print("Email address is: " + email)
+        }else{
+            alert(msg: "Success!")
+            print("Data is NOK")
+        }
+    }
     
-        print("First name is: "+fName!)
-        print("Last name is: "+lName!)
-        print("Email address is: "+email!)
+    func isDataOk(fName:String,lName:String,email:String)-> Bool{
+        if fName.isEmpty {
+            alert(msg: "First name is mandatory field")
+        }else if lName.isEmpty {
+            alert(msg: "Last name is mandatory field")
+        }else if email.isEmpty {
+            alert(msg: "Email address is mandatory field")
+        }else{
+          return true
+        }
+        return false
+    }
     
+    func alert(msg: String){
+        let alert = UIAlertController(title: "Alert", message: msg, preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
     }
     
 }
